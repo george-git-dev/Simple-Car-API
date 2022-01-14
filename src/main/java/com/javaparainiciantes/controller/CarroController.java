f81ae952cea4832ef6055ef597cb79661ae0f136
George <george_roots@hotmail.com>
2022-01-14 17:32:12 -0300

    Ajustes deploy

--- src/main/java/com/javaparainiciantes/controller/CarroController.java
+++ src/main/java/com/javaparainiciantes/controller/CarroController.java
@@ -3,11 +3,13 @@ package com.javaparainiciantes.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.javaparainiciantes.model.Carro;
@@ -17,6 +19,8 @@ import lombok.AllArgsConstructor;
 
 @RestController
 @AllArgsConstructor
+@RequestMapping("/api/v1")
+@CrossOrigin(allowedHeaders = "*", origins = "*")
 public class CarroController {
 
 	@Autowired
